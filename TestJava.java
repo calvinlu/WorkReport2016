a3ff12f02d6a3e84d2e3fd91b9be6e19345ca858
Calvin Lu <calvinlu2015@gmail.com>
2016-05-03 00:36:28 -0400

    #1 close java reverse array test

--- TestJava.java
+++ TestJava.java
@@ -1,21 +1,26 @@
-
+import java.util.Arrays;
 public class TestJava {
 
 	public static void main(String[] args) {
-		final int arraySize = 1000000;
+		final int arraySize = 100000000;
 		int[] array = new int[arraySize];
 
-		final long startTime = System.currentTimeMillis();
-
 		for (int i = 0 ; i < arraySize ; i++) {
-			array[i] = i;
-			System.out.println(array[i]);
+			array[i] = i + 1;
 		}
 
+		final long startTime = System.currentTimeMillis();
+		reverseArray(array);
 		final long endTime = System.currentTimeMillis();
 
-		System.out.println(endTime - startTime);
-
+		System.out.print("Array Reverse Time: " + (endTime - startTime) + "ms");
 	}
-	
+
+	private static void reverseArray(int[] array){
+		for (int i = 0 ; i < array.length / 2 ; i++) {
+			int temp = array[i];
+			array[i] = array[array.length - 1 - i];
+			array[array.length - 1 - i] = temp;
+		}
+	}
 }
