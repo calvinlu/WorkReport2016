4bb24b9da73b12ed22bf91898d3774c406fbd08c
Calvin Lu <calvinlu2015@gmail.com>
2016-05-02 23:43:49 -0400

    Start #1

---
+++ TestJava.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class TestJava {
+
+	public static void main(String[] args) {
+		//int array[] = 100000;
+		System.out.println("hello world");
+	}
+	
+}
