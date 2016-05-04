14a772866866ed44a72f120b3b83f2c771e327c0
Calvin Lu <calvinlu2015@gmail.com>
2016-05-04 18:26:22 -0400

    removed unused import

--- TestJava.java
+++ TestJava.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 public class TestJava {
 
 	public static void main(String[] args) {
@@ -23,4 +22,6 @@ public class TestJava {
 			array[array.length - 1 - i] = temp;
 		}
 	}
+
+
 }
