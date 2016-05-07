12a9dfb0a12d406dcd7ffa3d94add0aac9bba8d6
Calvin Lu <calvinlu2015@gmail.com>
2016-05-07 09:06:46 -0400

    python initial commit

--- java/IOJavaTest/src/IOJavaTest.java
+++ java/IOJavaTest/src/IOJavaTest.java
@@ -6,19 +6,21 @@ public class IOJavaTest {
 
     public static void main(String[] args) {
         String fileName = "DataFile.csv";
+        int count=0;
         try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
             //skip first line header
             String line = br.readLine();
             long startTime = System.currentTimeMillis();
             while ((line = br.readLine()) != null){
                 FileLine myFileLine = new FileLine(line);
+                count++;
             }
             long endTime = System.currentTimeMillis();
             System.out.println("IO Read Time: " + (endTime - startTime) + "ms");
+            System.out.println(count);
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
-
+    
 }
