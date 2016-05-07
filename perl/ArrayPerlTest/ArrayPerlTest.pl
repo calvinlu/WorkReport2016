a2424a54e4fccdcf8fb58c6db213e928d1113ee7
Calvin Lu <calvinlu2015@gmail.com>
2016-05-07 11:04:16 -0400

    Initial commit & perl array reverse code

---
+++ perl/ArrayPerlTest/ArrayPerlTest.pl
@@ -0,0 +1,27 @@
+use Time::HiRes qw(time);
+use POSIX qw(strftime);
+
+print "creating new array\n";
+my @array = createArray();
+
+print "reversing array\n";
+my $startTime = time();
+@array = reverseArray(\@array);
+my $endTime = time();
+my $runTime = ($endTime - $startTime) * 1000 ;
+print "Reverse Array Time: $runTime ms\n";
+
+sub reverseArray{
+   my @beforeArray = @{$_[0]};
+   my @afterArray = reverse @beforeArray;
+}
+
+
+sub createArray{
+	my $arraySize = 1000000;
+	my @newArray;
+	for my $i (0 .. $arraySize - 1) { 
+		push @newArray, $i;
+	}
+	return @newArray;
+}
